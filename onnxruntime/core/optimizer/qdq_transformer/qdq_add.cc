0438d60b7e3d1fa99c6b27a6600cc246fec2b7b7
Yufeng Li <liyufeng1987@gmail.com>
2021-03-15 13:24:37 -0700

    Add QDQ basic support

---
+++ onnxruntime/core/optimizer/qdq_transformer/qdq_add.cc
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <vector>
+
+#include "core/graph/graph.h"
+#include "core/optimizer/qdq_transformer/qdq_op_transformer.h"
+#include "core/optimizer/qdq_transformer/registry.h"
+
+namespace onnxruntime {
+class QDQAddTransformer : public QDQOperatorTransformer {
+ public:
+  QDQAddTransformer(Node& node, Graph& graph) : QDQOperatorTransformer(node, graph) {}
+
+  bool Transform(const std::vector<const Node*>& parents, const std::vector<const Node*>& children) override {
+    if (children.size() != 1) {
+      return false;
+    }
+    std::vector<NodeArg*> input_defs(graph_.GetNode(parents[0]->Index())->MutableInputDefs());
+    Node* b = graph_.GetNode(parents[1]->Index());
+    input_defs.insert(input_defs.end(), b->MutableInputDefs().begin(), b->MutableInputDefs().end());
+
+    Node* q = graph_.GetNode(children[0]->Index());
+    input_defs.push_back(q->MutableInputDefs()[1]);
+    input_defs.push_back(q->MutableInputDefs()[2]);
+
+    Node& qlinear_conv_node = graph_.AddNode(node_.Name(),
+                                             "QLinearAdd",
+                                             node_.Description(),
+                                             input_defs,
+                                             q->MutableOutputDefs(),
+                                             &node_.GetAttributes(),
+                                             kMSDomain);
+    qlinear_conv_node.SetExecutionProviderType(kCpuExecutionProvider);
+    return true;
+  }
+};
+
+DEFINE_QDQ_CREATOR(Add, QDQAddTransformer)
+
+}  // namespace onnxruntime
